601a8da8e4a2d62f018f4a550437b96068bf80f6
mikewon98a <mike@aquilaict.com>
2026-01-17 13:46:28 +0300

    Implement Repo Initaliztion

---
+++ app/constant/constant.ts
@@ -0,0 +1,8 @@
+export const ACTIONS: { [key: string]: string } = {
+  SELECT: "SELECT",
+  RECTANGLE: "RECTANGLE",
+  CIRCLE: "CIRCLE",
+  SCRIBBLE: "SCRIBBLE",
+  ARROW: "ARROW",
+  CLEAR: "CLEAR",
+};
